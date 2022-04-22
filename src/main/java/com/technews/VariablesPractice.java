adcc942f3f9fa9a6360a521239925f1c357b1383
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-04-21 22:21:22 -0500

    added user and post classes

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 3.5;
+        String c = "cat";
+
+        // calculated var sum is a double
+        double sum = a + b;
+
+        String concat = a + c;
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
